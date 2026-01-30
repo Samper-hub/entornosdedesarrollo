cbf4c23e6aa701e0729b32eace79b4750d5e5454
alumno <alumno@example.com>
2026-01-30 18:42:52 +0000

    Mensaje enviado desde Ubuntu

---
+++ Colaboracion.java
@@ -0,0 +1,6 @@
+public class Colaboracion{
+    public static void main(String[] args) {
+        System.out.println("Version inicial del proyecto.");
+        System.out.println("Mensaje desde Ubuntu.");
+    }
+}
